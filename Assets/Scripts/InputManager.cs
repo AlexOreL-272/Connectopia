88894a3f94c5d311eb98ac872eddb19b38c750ad
DogPierr <dankov.ns@phystech.edu>
2023-11-30 02:26:23 +0300

    Added simple road adding, without any cool things

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,41 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    [SerializeField]
-    private Camera sceneCamera;
+    [SerializeField] private Camera sceneCamera;
+
+    [SerializeField] private LayerMask placementLayermask;
 
     private Vector3 lastPosition;
 
-    [SerializeField]
-    private LayerMask placementLayermask;
+    public event Action OnClicked, OnExit;
 
-    // public event Action OnClicked, OnExit;
-
-    // private void Update()
-    // {
-    //     if(Input.GetMouseButtonDown(0))
-    //         OnClicked?.Invoke();
-    //     if(Input.GetKeyDown(KeyCode.Escape))
-    //         OnExit?.Invoke();
-    // }
+    private void Update()
+    {
+        if(Input.GetButton("Fire1"))
+            OnClicked?.Invoke();
+        if(Input.GetKeyDown(KeyCode.Escape))
+            OnExit?.Invoke();
+    }
 
     // public bool IsPointerOverUI()
     //     => EventSystem.current.IsPointerOverGameObject();
 
     public Vector3 GetSelectedMapPosition()
     {
-        Vector3 mousePos = Input.mousePosition;
+        var mousePos = Input.mousePosition;
         mousePos.z = sceneCamera.nearClipPlane;
-        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        var ray = sceneCamera.ScreenPointToRay(mousePos);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100, placementLayermask))
-        {
-            lastPosition = hit.point;
-            Debug.Log("a");
-        }
+        if (Physics.Raycast(ray, out hit, 100, placementLayermask)) lastPosition = hit.point;
         return lastPosition;
     }
 }
--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -1,16 +1,55 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
-    private InputManager inputManager;
+    private ObjectsDatabaseSO database;
+
+    [SerializeField] private InputManager inputManager;
 
     [SerializeField] private GameObject mouseIndicator;
-    void Update()
+
+    [SerializeField] private Grid _grid;
+
+    [SerializeField] private int selectedObject = 0;
+
+    private Dictionary<Vector3Int, bool> _isBusy = new Dictionary<Vector3Int, bool>(); 
+    
+    private void Start()
     {
-        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
-        mouseIndicator.transform.position = mousePosition;
+        inputManager.OnClicked += PlaceObject;
     }
-}
+
+    private void Update()
+    {
+        var mousePosition = inputManager.GetSelectedMapPosition();
+        var gridPosition = _grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = _grid.CellToWorld(gridPosition);
+    }
+    
+    public void PlaceObject()
+    {
+        var mousePosition = inputManager.GetSelectedMapPosition();
+        var gridPosition = _grid.WorldToCell(mousePosition);
+        if (IfBusy(gridPosition))
+        {
+            return;
+        }
+        GameObject newObject = Instantiate(database.objectsData[selectedObject].Prefab);
+        newObject.transform.position = _grid.CellToWorld(gridPosition) + database.objectsData[selectedObject].Offset;
+        _isBusy[gridPosition] = true;
+    }
+
+    private bool IfBusy(Vector3Int position)
+    {
+        bool value;
+        if (_isBusy.TryGetValue(position, out value))
+        {
+            return value;
+        }
+
+        return false;
+    }
+}//
