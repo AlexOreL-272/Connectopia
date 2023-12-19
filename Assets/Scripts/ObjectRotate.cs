3941faa4162720389f9f17fc3ec756a060b289c9
Aidan Kiresheev <114140985+Daksio@users.noreply.github.com>
2023-12-19 22:14:34 +0300

    Add R bind
    yes

--- Assets/Scripts/ObjectRotate.cs
+++ Assets/Scripts/ObjectRotate.cs
@@ -10,10 +10,16 @@ public class ObjectRotate : MonoBehaviour {
     public float minZoom = 2.0f;
     public float maxZoom = 20.0f;
 
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private float initialOrthographicSize;
     private Camera cam;
 
     void Start() {
         cam = GetComponent<Camera>();
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        initialOrthographicSize = cam.orthographicSize;
     }
 
     void Update() {
@@ -24,13 +30,15 @@ public class ObjectRotate : MonoBehaviour {
         if (Mathf.Abs(horizontalInput) > 0 || Mathf.Abs(verticalInput) > 0) {
             float horizontal = rotationSpeed * Input.GetAxis("Horizontal");
             float vertical = rotationSpeed * Input.GetAxis("Vertical");
-
+            
             transform.RotateAround(target.position, Vector3.up, -horizontal);
             transform.RotateAround(target.position, transform.right, vertical);
 
-            if (transform.position.y < minY) {
+            if (transform.position.y < minY)
+            {
                 transform.position = new Vector3(transform.position.x, minY, transform.position.z);
             }
+            
 
             transform.LookAt(target);
         }
@@ -38,5 +46,12 @@ public class ObjectRotate : MonoBehaviour {
         float newSize = cam.orthographicSize - zoomInput * zoomSpeed;
         newSize = Mathf.Clamp(newSize, minZoom, maxZoom);
         cam.orthographicSize = newSize;
+        
+        if (Input.GetKeyDown(KeyCode.R)) {
+            transform.position = initialPosition;
+            transform.rotation = initialRotation;
+            cam.orthographicSize = initialOrthographicSize;
+        }
+        
     }
 }
