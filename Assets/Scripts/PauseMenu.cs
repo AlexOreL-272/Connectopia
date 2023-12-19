f26c0f465bf5e241a97026f008f3eb440746eb85
thereayou44 <gorbatsevich.na@phystech.edu>
2023-12-19 05:36:12 +0300

    Сделал базовое главное меню  и меню пазуы, добавил бг

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenu;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+       GameIsPaused = true;
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
